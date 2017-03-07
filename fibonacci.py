b9d825ed4fc35112b7745ce0d78b63456e1987a7
Chike Nwaenie <chikenwaenie@gmail.com>
2017-03-07 17:40:52 +0000

    Update fibonacci.py
    Updated for simplicity

--- fibonacci.py
+++ fibonacci.py
@@ -1,21 +1,9 @@
-param1 = input("First number: ")
-param2 = input("How many iterations: ")
-
 def fibonacci(first, limit):
-    import sys
-    try:
-        if any([first < 0, first == 0, limit < 0, limit == 0]):
-            return -1
-        else:
-            x = first
-            y = first + 1
-            for i in range(limit):
-                print(x)
-                x, y = y, x+y
-    except TypeError:
-        sys.stdout.write("Make sure both your values are integers")
+    x = first
+    y = first + 1
+    for i in range(limit):
+        print(x)
+        x, y = y, x+y
 
-param1 = int(param1)
-param2 = int(param2)        
 
-fibonacci(param1, param2)
+fibonacci(1, 10)
